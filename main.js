bb6a34f9a8b0c338e48dac302bd9e764645a5ec5
Victor_Dev <165293570+VictorHMA@users.noreply.github.com>
2024-04-22 17:23:38 -0300

    main.js

--- main.js
+++ main.js
@@ -6,8 +6,12 @@ const btnReset = document.querySelector("#btnReset")
 let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
+
+
+
 // Eventos
-btnTry.addEventListener('click', handleTryClick)
+btnTry.addEventListener('click', handleTryClick) 
+// addEventListener - addEvent = adiciona o nome do evento ('x', )| Listener = para quando eu lidar com ( , x)
 btnReset.addEventListener('click', handleResetClick)
 document.addEventListener('keydown', function(e) {
     if(e.key == 'Enter' && screen1.classList.contains('hide')) {
@@ -17,15 +21,30 @@ document.addEventListener('keydown', function(e) {
 
 // funções
 function handleTryClick(event) {
-    event.preventDefault() // nao faca o padrao
+    event.preventDefault() /* se deixasse sem declarar 
+    o prevent id ele iria fazer o padrão de um botão dentro
+    de um formulário que é enviar os dados, mas como 
+    declarei que NÃO é pra fazer o padrão não acontece nada,
+    as informações ficam na página (preventDefault = evitar o
+    padrão)*/
 
     const inputNumber = document.querySelector("#inputNumber")
 
     if(Number(inputNumber.value) == randomNumber) {
         toggleScreen()
-        screen2.querySelector("h2").innerText = `acertou em ${xAttempts} tentativas`
+        screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas, o número era ${randomNumber}`
     }
+    
+    if (Number(inputNumber.value) < 0 || Number(inputNumber.value) > 10) {
+        alert("Por favor insira um número de 0 a 10!")
+        }
 
+    else if (inputNumber.value == "") {
+            alert("Digite um número valido, não aceita vazio");
+            inputNumber.value = "";
+            xAttempts = 0;
+            }
+    
     inputNumber.value = ""
     xAttempts++
 }
